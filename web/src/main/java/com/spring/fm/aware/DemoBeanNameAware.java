32bcbf07eadbcc6a376120984c9ffb8fafa0b594
jiyang <jiyang@gozap.com>
2018-07-18 18:21:23 +0800

    spring event 事件监听

---
+++ web/src/main/java/com/spring/fm/aware/DemoBeanNameAware.java
@@ -0,0 +1,10 @@
+package com.spring.fm.aware;
+
+/***
+ * @Des: 场景式存证_Demo
+ * @Author: jiyang
+ * @Date: 2018-07-18 17:54
+ */
+public class DemoBeanNameAware {
+
+}
---
+++ web/src/main/java/com/spring/fm/aware/Test.java
@@ -0,0 +1,10 @@
+package com.spring.fm.aware;
+
+/***
+ * @Des: 场景式存证_Demo
+ * @Author: jiyang
+ * @Date: 2018-07-18 17:56
+ */
+public class Test {
+
+}
